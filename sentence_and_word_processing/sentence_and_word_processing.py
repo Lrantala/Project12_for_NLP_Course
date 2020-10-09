537740d92939df795b8e7421b5bc4eaf7f64dc68
Leevi Rantala <leevi.rantala@oulu.fi>
2020-10-09 14:57:41 +0300

    Some change to preprocessing and more tests

--- sentence_and_word_processing/sentence_and_word_processing.py
+++ sentence_and_word_processing/sentence_and_word_processing.py
@@ -2,22 +2,29 @@ from nltk.tokenize import word_tokenize
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords as nltk_stopwords
 
-def preprocess(sentence, use_stemmer=False, use_lowercase=False, use_stopwords = False):
+def preprocess(sentence, use_stemmer=False, use_lowercase=False, use_stopwords = False, remove_nonalpha=False):
+    # We always tokenize
+    words = word_tokenize(sentence)
+
+
     if use_stopwords:
         stopwords = list(set(nltk_stopwords.words('english')))
     else:
         stopwords = []
 
-    # We always tokenize
-    words = word_tokenize(sentence)
+    # Using just stop words without preprocessing to lower makes little sense, as it doesn't find all of them then
+    words = [word for word in words if word not in stopwords]
 
     if use_lowercase:
-        words = [word.lower() for word in words if
-                 word.isalpha() and word not in stopwords]
+        words = [word.lower() for word in words if word.lower() not in stopwords]
 
+    # Note that this also removes all words, which have a number in them
+    if remove_nonalpha:
+        words = [word for word in words if
+                 word.isalpha() and word not in stopwords]
     # We stem if asked to, although it will cause problems with synset searches
+    # Stemmer also makes words lowercase by default
     if use_stemmer:
-        stemmer = SnowballStemmer(
-            "english")
+        stemmer = SnowballStemmer("english")
         words = [stemmer.stem(word) for word in words]
     return words
--- tests/test_sentence_and_word_processing.py
+++ tests/test_sentence_and_word_processing.py
@@ -1,8 +1,57 @@
 import sentence_and_word_processing.sentence_and_word_processing as swp
 
-def test_preprocessing_tolower():
+def test_preprocessing_tokenize():
     test_sentence = "This is A TEST SENtence, to Make Sure Lower works"
-    result_sentence = ["this", "is", "a", "test", "sentence", "to", "make", "sure", "lower", "works"]
-    preprocessed_sentence = swp.preprocess(sentence=test_sentence, use_stemmer=False, use_lowercase=True,use_stopwords=False)
+    result_sentence = ["This", "is", "A", "TEST", "SENtence", ",", "to", "Make", "Sure", "Lower", "works"]
+    preprocessed_sentence = swp.preprocess(sentence=test_sentence, use_stemmer=False,
+                                           use_lowercase=False, use_stopwords=False, remove_nonalpha=False)
     assert result_sentence == preprocessed_sentence
 
+def test_preprocessing_tolower():
+    test_sentence = "This is A TEST SENtence, to Make Sure Lower works"
+    result_sentence = ["this", "is", "a", "test", "sentence", ",", "to", "make", "sure", "lower", "works"]
+    preprocessed_sentence = swp.preprocess(sentence=test_sentence, use_stemmer=False,
+                                           use_lowercase=True, use_stopwords=False, remove_nonalpha=False)
+    assert result_sentence == preprocessed_sentence
+
+def test_preprocessing_remove_stopwords():
+    test_sentence = "This is A TEST SENtence, to Make Sure Lower works"
+    result_sentence = ["This", "A", "TEST", "SENtence", ",", "Make", "Sure", "Lower", "works"]
+    preprocessed_sentence = swp.preprocess(sentence=test_sentence, use_stemmer=False,
+                                           use_lowercase=False, use_stopwords=True, remove_nonalpha=False)
+    assert result_sentence == preprocessed_sentence
+
+def test_preprocessing_stem():
+    test_sentence = "This is A TEST SENtence, to Make Sure Lower works"
+    result_sentence = ["this", "is", "a", "test", "sentenc", ",", "to", "make", "sure", "lower", "work"]
+    preprocessed_sentence = swp.preprocess(sentence=test_sentence, use_stemmer=True,
+                                           use_lowercase=False, use_stopwords=False, remove_nonalpha=False)
+    assert result_sentence == preprocessed_sentence
+
+def test_preprocessing_tolower_remove_nonalpha():
+    test_sentence = "This is A TEST SENtence, to Make Sure Lower works"
+    result_sentence = ["this", "is", "a", "test", "sentence", "to", "make", "sure", "lower", "works"]
+    preprocessed_sentence = swp.preprocess(sentence=test_sentence, use_stemmer=False,
+                                           use_lowercase=True, use_stopwords=False, remove_nonalpha=True)
+    assert result_sentence == preprocessed_sentence
+
+def test_preprocessing_tokenize_remove_nonalpha():
+    test_sentence = "This is A TEST SENtence, to Make6 Sure Lower works"
+    result_sentence = ["This", "is", "A", "TEST", "SENtence", "to", "Sure", "Lower", "works"]
+    preprocessed_sentence = swp.preprocess(sentence=test_sentence, use_stemmer=False,
+                                           use_lowercase=False, use_stopwords=False, remove_nonalpha=True)
+    assert result_sentence == preprocessed_sentence
+
+def test_preprocessing_lower_remove_stopwords():
+    test_sentence = "This is A TEST SENtence, to Make6 Sure Lower works"
+    result_sentence = ["test", "sentence", ",", "make6", "sure", "lower", "works"]
+    preprocessed_sentence = swp.preprocess(sentence=test_sentence, use_stemmer=False,
+                                           use_lowercase=True, use_stopwords=True, remove_nonalpha=False)
+    assert result_sentence == preprocessed_sentence
+
+def test_preprocessing_lower_remove_stopwords_and_nonalpha():
+    test_sentence = "This is A TEST SENtence, to Make6 Sure Lower works"
+    result_sentence = ["test", "sentence", "sure", "lower", "works"]
+    preprocessed_sentence = swp.preprocess(sentence=test_sentence, use_stemmer=False,
+                                           use_lowercase=True, use_stopwords=True, remove_nonalpha=True)
+    assert result_sentence == preprocessed_sentence
