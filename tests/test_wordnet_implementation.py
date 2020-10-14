a050c26958058c50c75156891c47f9ae5ad36d76
Leevi Rantala <leevi.rantala@oulu.fi>
2020-10-14 15:00:22 +0300

    Added path similarity function for synset lists
    Added a function calculate path similarities between synset
    lists. Takes two lists, and compares them synset by synset, and
    returns the highest matching score.
    
    If no match is found, then -1 is returned.

--- tests/test_wordnet_implementation.py
+++ tests/test_wordnet_implementation.py
@@ -4,7 +4,7 @@ def test_return_whole_synset_list_from_word():
     word = "computer"
     # Testing here just that the synset names are right, if they are the correct synsets are returned
     test_computer_synsets = ["computer.n.01", "calculator.n.01"]
-    computer_synsets = wni.return_synset_list_for_word(word, whole_synset=True)
+    computer_synsets = wni.retrieve_synset_list_for_word(word, whole_synset=True)
     computer_synset_name_list = [name.name() for name in computer_synsets]
     assert test_computer_synsets == computer_synset_name_list
 
@@ -12,16 +12,16 @@ def test_return_whole_synset_list_from_word():
 def test_return_name_synset_list_from_word():
     word = "computer"
     test_computer_synsets = ["computer.n.01", "calculator.n.01"]
-    computer_synsets = wni.return_synset_list_for_word(word, whole_synset=False)
+    computer_synsets = wni.retrieve_synset_list_for_word(word, whole_synset=False)
     assert test_computer_synsets == computer_synsets
 
 
 def test_return_hyponym_list_for_synset():
-    computer_synset = wni.return_synset_list_for_word("computer")
+    computer_synset = wni.retrieve_synset_list_for_word("computer")
     computer_synset_1 = computer_synset[0]
-    computer_hyponyms = wni.return_hyponym_or_hypernym_list_for_synset(computer_synset_1,
-                                                                       whole_synset=False,
-                                                                       hypo_or_hyper=0)
+    computer_hyponyms = wni.retrieve_hyponym_or_hypernym_list_for_synset(computer_synset_1,
+                                                                         whole_synset=False,
+                                                                         hypo_or_hyper=0)
     computer_hyponyms_checklist = ["analog_computer.n.01", "digital_computer.n.01",
                                    "home_computer.n.01", "node.n.08", "number_cruncher.n.02",
                                    "pari-mutuel_machine.n.01", "predictor.n.03", "server.n.03",
@@ -30,10 +30,24 @@ def test_return_hyponym_list_for_synset():
 
 
 def test_return_hypernym_list_for_synset():
-    computer_synset = wni.return_synset_list_for_word("computer")
+    computer_synset = wni.retrieve_synset_list_for_word("computer")
     computer_synset_1 = computer_synset[0]
-    computer_hyponyms = wni.return_hyponym_or_hypernym_list_for_synset(computer_synset_1,
-                                                                       whole_synset=False,
-                                                                       hypo_or_hyper=1)
+    computer_hyponyms = wni.retrieve_hyponym_or_hypernym_list_for_synset(computer_synset_1,
+                                                                         whole_synset=False,
+                                                                         hypo_or_hyper=1)
     computer_hyponyms_checklist = ["machine.n.01"]
     assert computer_hyponyms == computer_hyponyms_checklist
+
+
+def test_calculate_path_similarity_for_synset_lists():
+    computer_synset = wni.retrieve_synset_list_for_word("computer")
+    analog_computer_synset = wni.retrieve_synset_list_for_word("analog_computer")
+    similarity_score = wni.calculate_path_similarity_for_synset_lists(computer_synset, analog_computer_synset)
+    assert similarity_score == 0.5
+
+
+def test_calculate_path_similarity_for_synset_lists():
+    computer_synset = wni.retrieve_synset_list_for_word("cat")
+    analog_computer_synset = wni.retrieve_synset_list_for_word("dog")
+    similarity_score = wni.calculate_path_similarity_for_synset_lists(computer_synset, analog_computer_synset)
+    assert similarity_score == 0.2
--- wordnet_implementation/wordnet_implementation.py
+++ wordnet_implementation/wordnet_implementation.py
@@ -1,7 +1,7 @@
 from nltk.corpus import wordnet as wn
 
 
-def return_synset_list_for_word(word, whole_synset=True):
+def retrieve_synset_list_for_word(word, whole_synset=True):
     """Returns the synset list for a word. Possibility to return whole synsets or just names
     of synsets."""
     if whole_synset:
@@ -10,7 +10,7 @@ def return_synset_list_for_word(word, whole_synset=True):
         return [name.name() for name in wn.synsets(word)]
 
 
-def return_hyponym_or_hypernym_list_for_synset(synset, whole_synset=True, hypo_or_hyper=0):
+def retrieve_hyponym_or_hypernym_list_for_synset(synset, whole_synset=True, hypo_or_hyper=0):
     """Returns the hyponym synset list for a synset. Possibility to return whole synsets or just names
     of synsets. Parameter hypo_or_hyper takes either 0 for hyponyms, or 1 for hypernyms. Default: 0"""
     if hypo_or_hyper == 1:
@@ -25,4 +25,20 @@ def return_hyponym_or_hypernym_list_for_synset(synset, whole_synset=True, hypo_o
     else:
         return [name.name() for name in synset_list]
 
+
+def calculate_path_similarity_for_synset_lists(synset_list1, synset_list2):
+    """Take two lists of synsets, calculate the path similarities between all permutations in them, and
+    return the highest score as the similarity score."""
+
+    # Set highest score to -1, which is given if there's no matches at all in path similarities
+    highest_score = -1
+
+    for synset1 in synset_list1:
+        for synset2 in synset_list2:
+            path_score = synset1.path_similarity(synset2)
+            if path_score:
+                if path_score > highest_score:
+                    highest_score = path_score
+
+    return highest_score
 
