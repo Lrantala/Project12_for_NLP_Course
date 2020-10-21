c36ecb8d2e833693af16d2154ff7d32400d36e0a
Leevi Rantala <leevi.rantala@oulu.fi>
2020-10-21 09:09:40 +0300

    Fixed a bug relating to verb hyponyms
    Verb hyponym union was calculated wrong, it took
    separately both sentences and added the resutls together,
    when it should have added both sentences together and
    after that looked for the union. Fixed.

--- custom_similarity_measure/custom_similarity_measure.py
+++ custom_similarity_measure/custom_similarity_measure.py
@@ -39,7 +39,7 @@ def custom_similarity_measure(synset1, synset2):
         noun_hyper = common_hyper/total_hyper
 
     common_hypo = commonality(synset1_hypo, synset2_hypo)
-    total_hypo = hyp_union(synset1_hypo) + hyp_union(synset2_hypo)
+    total_hypo = hyp_union(synset1_hypo + synset2_hypo)
     # total_hypo = len(set().union(synset1_hypo + synset2_hypo))
     if total_hypo is not 0:
         verb_hypo = common_hypo / total_hypo
@@ -47,12 +47,13 @@ def custom_similarity_measure(synset1, synset2):
     return noun_hyper, verb_hypo # to be returned to the function that makes the call for the custom similarity measure
 
 def commonality(synset1, synset2):
-    count = 0
+    union_list = []
     for item1 in synset1:
         for item2 in synset2:
                if item1 == item2 and item1==item2 != []:
-                   count += 1
-    return count
+                   if item1 not in union_list:
+                       union_list.append(item1)
+    return len(union_list)
 
 def hyp_union(hyp_list):
     union_list = []
